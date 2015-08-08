78d71f7fade1ff5cb74660245bd6567483d42d86
Nasr Maswood <nasrmaswood@gmail.com>
2015-08-08 01:21:26 -0500

    began directives and inserting content in arabic

--- client/app/js/controllers/exercises.controller.js
+++ client/app/js/controllers/exercises.controller.js
@@ -1,31 +1,35 @@
+// jshint ignore: start
 'use strict';
 
+
 angular.module('arabicApp')
-  .controller('ExercisesCtrl',['$scope',function ($scope) {
+  .controller('ExercisesCtrl',['$scope','$http', function ($scope,$http) {
 
     $scope.exercises = {};
 
-    console.log('Hey does this work?');
 
+    $scope.readings = [{
+      "unit" : 1,
+      "title" : 'ابن بطوطة و رحلة في التاريخ',
+      "content" : ''
+    }];
 
-    $scope.exercises.quizQuestions = [
-      {
-        question : 'How big is the moon?',
-        option1 : '',
-        option2 : '',
-        option3 : '',
-        correct : ''
-      }
-    ];
+    $scope.exercises.quizQuestions =
+    [{
+        "question": "What is this",
+        "option1": "A",
+        "option2": "B",
+        "option3": "C"
+      }, {
+      "question": "What is here",
+      "option1": "A",
+      "option2": "B",
+      "option3": "C"
+      }, {
+      "question": "Wht is there",
+      "option1": "A",
+      "option2": "B",
+      "option3": "C"
+      }];
 
-
-
-
-
-
-
-
-  }]);
-
-
-
+    }]);
