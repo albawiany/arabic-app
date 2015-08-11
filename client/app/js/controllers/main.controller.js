8a8aaee09b18a135ed1aa34c59c5efe32bee178f
Nasr Maswood <nasrmaswood@gmail.com>
2015-08-11 14:45:47 -0500

    square 1

--- client/app/js/controllers/main.controller.js
+++ client/app/js/controllers/main.controller.js
@@ -1,13 +1,20 @@
 'use strict';
 
 angular.module('arabicApp')
-  .controller('MainCtrl', ['$scope','$http','socket', function ($scope, $http, socket) {
-    $scope.awesomeThings = [];
+  .controller('MainCtrl', ['$scope','ngAudio', function ($scope,ngAudio) {
 
-    $http.get('/api/things').success(function (awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-      socket.syncUpdates('thing', $scope.awesomeThings);
-    });
+    $scope.audio1 = ngAudio.load("http://k001.kiwi6.com/hotlink/2m45pypi9f/Fetty_Wap_679_feat._Remy_Boyz_Official_Video_.mp3");
+    $scope.playSound = function(){
+      $scope.audio1.play();
+    };
+
+    console.log($scope.audio1);
+
+
+    var url = 'audio/song1.mp3';
+
+    $scope.audio = ngAudio.load(url);
+    $scope.audio.volume = 0.8;
 
 
   }]);
